386246e0c19d1d7a2c6c6d3910ce47a1c64c4be3
Przemek Tomaka <przemo3028@gmail.com>
2022-09-11 11:21:28 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
         },
         {
             content: "Iść do domu",
-            done: false,
+            done: true,
         },
         {
             content: "iść spać",
@@ -19,18 +19,42 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
             ${task.content}
-            </li>
+            </li> 
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
-    const init = () => {
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
         render();
     };
-    
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        };
+
+        addNewTask(newTaskContent);
+    }
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+    };
+
     init();
 }
